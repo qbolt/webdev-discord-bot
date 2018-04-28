fde99ee8b2a8fa8e403e27215e3f92618dd4c38c
Quinton Bolt <bolt.quinton@gmail.com>
2018-04-28 01:33:13 -0500

    Added linter and fixed linting issues

--- roles.js
+++ roles.js
@@ -8,33 +8,33 @@ const FSN = 'FSN'
 const roles = {
     FED: {
         id: '439613061725159424',
-        welcome: 'You have been added to the Front-End Dev role!'
+        welcome: 'You have been added to the Front-End Dev role!',
     },
     BED: {
         id: '439612764416245760',
-        welcome: 'You have been added to the Back-End Dev role!'
+        welcome: 'You have been added to the Back-End Dev role!',
     },
     FSD: {
         id: '439613218965422083',
-        welcome: 'You have been added to the Full Stack Dev role!'
+        welcome: 'You have been added to the Full Stack Dev role!',
     },
     FEN: {
         id: '439613261470629889',
-        welcome: 'You have been added to the Front-End Newbie role!'
+        welcome: 'You have been added to the Front-End Newbie role!',
     },
     BEN: {
         id: '439613357360807936',
-        welcome: 'You have been added to the Back-End Newbie role!'
+        welcome: 'You have been added to the Back-End Newbie role!',
     },
     FSN: {
         id: '439613546083647489',
-        welcome: 'You have been added to the Full Stack Newbie role!'
-    }
+        welcome: 'You have been added to the Full Stack Newbie role!',
+    },
 }
 
 const hierarchy = {
     FSD: [ FED, BED ],
-    FSN: [ FEN, BEN ]
+    FSN: [ FEN, BEN ],
 }
 
 const getRoles = (role) => {
@@ -53,8 +53,8 @@ const getIncludedRoles = (role) => {
 
 const applyRoles = (role, message) => {
     getRoles(role.toUpperCase()).forEach(r => {
-        message.member.addRole(r.id);
-        message.reply(r.welcome);
+        message.member.addRole(r.id)
+        message.reply(r.welcome)
     })
 }
 
