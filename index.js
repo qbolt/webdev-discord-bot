00832514839809bb8562d7dbc45a2370f8b5dc64
Quinton Bolt <bolt.quinton@gmail.com>
2018-04-28 01:25:10 -0500

    Implemented hierarchical role joining and restructuring

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const config = require('./config.json');
+const applyRoles = require('./roles')
+const PREFIX = '$';
+
+client.on('ready', () => {
+	console.log('Bot is online!');
+});
+
+client.on('message', message => {
+	const args = message.content.substring(PREFIX.length).split(" ");
+	const command = args[0]
+
+	applyRoles(command, message)
+
+	if(command === 'help') {
+		message.reply('To assign yourself a role please use the following commands. Front-End Dev: $FED, Back-End Dev: $BED, Full Stack Dev: $FSD, Front-End Newbie: $FEN, Back-End Newbie: $BEN, Full Stack Newbie: $FSN. Please choose only ONE role!');
+	}
+});
+
+client.login(config.BOT_TOKEN);
---
+++ roles.js
@@ -0,0 +1,61 @@
+const FED = 'FED'
+const BED = 'BED'
+const FSD = 'FSD'
+const FEN = 'FEN'
+const BEN = 'BEN'
+const FSN = 'FSN'
+
+const roles = {
+    FED: {
+        id: '439613061725159424',
+        welcome: 'You have been added to the Front-End Dev role!'
+    },
+    BED: {
+        id: '439612764416245760',
+        welcome: 'You have been added to the Back-End Dev role!'
+    },
+    FSD: {
+        id: '439613218965422083',
+        welcome: 'You have been added to the Full Stack Dev role!'
+    },
+    FEN: {
+        id: '439613261470629889',
+        welcome: 'You have been added to the Front-End Newbie role!'
+    },
+    BEN: {
+        id: '439613357360807936',
+        welcome: 'You have been added to the Back-End Newbie role!'
+    },
+    FSN: {
+        id: '439613546083647489',
+        welcome: 'You have been added to the Full Stack Newbie role!'
+    }
+}
+
+const hierarchy = {
+    FSD: [ FED, BED ],
+    FSN: [ FEN, BEN ]
+}
+
+const getRoles = (role) => {
+    const selectedRole = roles[role]
+    return selectedRole
+        ? getIncludedRoles(role).concat(selectedRole)
+        : []
+}
+
+const getIncludedRoles = (role) => {
+    const included = hierarchy[role]
+    return included
+        ? included.map(r => hierarchy[r] ? getIncludedRoles(r) : roles[r])
+        : []
+}
+
+const applyRoles = (role, message) => {
+    getRoles(role.toUpperCase()).forEach(r => {
+        message.member.addRole(r.id);
+        message.reply(r.welcome);
+    })
+}
+
+module.exports = applyRoles
