26297d7f0bef35c0f7d26cf5c9e4e71512c6308d
Zaekkor <jason1crowe@gmail.com>
2018-04-28 17:52:26 -0400

    Added a welcome message for new members of server to be directed to role assignment.

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ client.on('ready', () => {
 	console.log('Bot is online!')
 })
 
+client.on('guildMemberAdd', member => {
+   member.send("Welcome to the server! Please stop by #role-assignment to assign a role based on your skill level! You can use $help to see the commands.");
+});
+
 client.on('message', message => {
 	const args = message.content.substring(PREFIX.length).split(' ')
 	const command = args[0]
